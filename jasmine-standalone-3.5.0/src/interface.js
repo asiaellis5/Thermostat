34fa4557f87ec6f07a21708fe8c4e8af6b88f1e0
Asia <asiaellis5@gmail.com>
2020-01-24 11:31:22 +0000

    styling and audio

--- jasmine-standalone-3.5.0/src/interface.js
+++ jasmine-standalone-3.5.0/src/interface.js
@@ -1,16 +1,23 @@
 $(document).ready(function() {
   var thermostat = new Thermostat() 
 
-  $("#temperature").text(thermostat.temperature);
+  updateTemperature();
 
   $("#temperature-up").on('click', function() {
     thermostat.increaseTemperature();
     updateTemperature();
+    $('#audioHot').trigger('play');
+    if (thermostat.temperature > 24) {
+      updateFire() 
+    } 
   })
 
   $('#temperature-down').on('click', function(){
     thermostat.decreaseTemperature();
     updateTemperature();
+    if (thermostat.temperature > 24) {
+      updateFire()
+    }
   })
 
   $('#temperature-reset').on('click', function(){
@@ -21,6 +28,9 @@ $(document).ready(function() {
   $('#powersave').on('click', function(){
     thermostat.turnPowerSavingOff();
     updateTemperature();
+    if (thermostat.temperature > 24) {
+      updateFire()
+    }
   })
 
 function updateTemperature() {
@@ -28,9 +38,20 @@ function updateTemperature() {
   $('#temperature').attr('class', thermostat.currentEnergyUsage())
   }
 
-  $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=31c50239396d81ea3e312fc1c9c788ab&units=metric', function(data){
-    $("#current-temperature").text(data.main.temp);
+  $('#current-city').change(function() {
+    var city = $('#current-city').val();
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=31c50239396d81ea3e312fc1c9c788ab&units=metric', function(data){
+      $("#current-temperature").text(data.main.temp);
+    })
   })
+
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=31c50239396d81ea3e312fc1c9c788ab&units=metric', function(data) {
+    $('#london').text(data.main.temp);
+  })
+
+  function updateFire() {
+    $('#temperature').attr('class', "font-effect-fire-animation");
+  }
 
 
 });
